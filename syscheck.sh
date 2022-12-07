892334569a99018bd76591ba8d93d18f479e2d86
Cyneric400 <davidrsharpe@southern.edu>
2022-12-07 15:43:37 -0500

    adding reactor checking

--- syscheck.sh
+++ syscheck.sh
@@ -8,6 +8,15 @@ nav1=OFFLINE
 mainenginefile="engine_control/engine_configuration/main_engines"
 nav0file="engine_control/engine_configuration/nav_thruster_0"
 nav1file="engine_control/engine_configuration/nav_thruster_1"
+reactorfile="reactor_system/power_alloc.config.txt"
+
+enginePowerStatus=0
+navigationStatus=0
+engineOnline=0
+
+meStatus=0
+nav0Status=0
+nav1Status=0
 
 # add in more checkers that will set the thruster files to enabled if all the conditions are met (reactor file has been edited, nav system has been repaired)
 
@@ -39,13 +48,30 @@ done < "$nav1file"
 
 # Set up a way to have the engine checker pull the systems status from this file.
 
+while IFS= read -r line
+do
+	if [ "$line" == "engine-controllers=25" ];
+	then
+		enginePowerStatus=1
+                echo "$enginePowerStatus"
+	fi
+done < "$reactorfile"
+
 export $mainengstatus
 
 echo "Running systems check..."
 sleep 1
 echo "Navigation System $navStatus ..."
 sleep 0.1
-echo "Reactor Systems $reactorStatus..."
+# echo "Reactor Systems $reactorStatus..."
+
+if [ $enginePowerStatus == 1 ];
+then
+	echo "Engine controllers online"
+else
+	echo "WARNING: Insufficient power. Unable to bring engine controllers online."
+fi
+
 sleep 0.1
 # echo "Engine Systems $engsysStatus..."
 # sleep 0.1
