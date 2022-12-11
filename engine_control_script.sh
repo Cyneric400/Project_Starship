68ef4f630352ded183aa6eab912a00e7e45578a3
Cyneric400 <davidrsharpe@southern.edu>
2022-12-11 13:16:08 -0500

    solving challenge and tweaking script

--- engine_control_script.sh
+++ engine_control_script.sh
@@ -84,6 +84,7 @@ then
 	orbit_file_status=1
 fi
 
+# echo "$compass_line_status, $copied_file_status, $useless_file_0_status, $orbit_file_status"
 # Checks to see if each challenge has been completed
 if [[ "$compass_line_status" == 1 ]] && [[ "$copied_file_status" == 1 ]] && [[ "$useless_file_0_status" == 1 ]] && [[ "$orbit_file_status" == 1 ]];
 then
